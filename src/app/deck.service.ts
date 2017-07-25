d5eb752db124c59885f530b2846cf009ce0f8c01
Jordan Loop and kimlan nguyen <jordan+kimlan@>
2017-07-25 09:49:04 -0700

    Refactor, move code from gameboard to deck.service

--- src/app/deck.service.ts
+++ src/app/deck.service.ts
@@ -4,15 +4,84 @@ import { Creature } from './creature.model';
 
 @Injectable()
 export class DeckService {
+  actionCard;
   creature: Creature;
   player: Player;
+  deck: any[] = [];
+  shuffleDeck: any[] = [];
+  localPlayers: Player[] = [];
 
   constructor() { }
+
+  //get cards from firebase
+  getCards(cards: any){
+    //Retrieve cards from firebase
+    for(let card of cards[0][0].attackCards){
+      for(let i = 1; i < 26; i++){
+        this.deck.push(card);
+      }
+    }
+    for(let card of cards[0][1].actionCards){
+      for(let i = 1; i < 8; i++){
+        this.deck.push(card);
+      }
+    }
+    for(let card of cards[0][2].itemCards){
+      for(let i = 1; i < 8; i++){
+        this.deck.push(card);
+      }
+    }
+    //Populate deck
+    while(this.deck.length > 0){
+      let i = this.deck.length;
+      var randomNumber = Math.floor(Math.random() * i);
+      var singleCard = this.deck[randomNumber];
+      this.shuffleDeck.push(singleCard);
+      this.deck.splice(this.deck.indexOf(singleCard), 1);
+    }
+      return this.shuffleDeck;
+  }
+
+  //set cards in play
+  setCardInPlay(card: any, player: Player){
+    //use potion card
+    if(card.name == "Potion Of Healing"){
+      if(player.hp == 100){
+        alert("This player's hp is full.");
+      }
+      else
+      {
+        player.hp += 25;
+        if(player.hp > 100){
+          player.hp = 100;
+        }
+        player.hand.splice(player.hand.indexOf(card), 1);
+      }
+    }
+    //set action card
+    else if(card.name == "Edge Out" || card.name == "Mixed Signal"){
+      this.actionCard = card;
+      alert("Select a target.");
+      player.hand.splice(player.hand.indexOf(card), 1);
+    }
+    //set attack card
+    else if(card.name == "Attack 40"){
+      player.hand.splice(player.hand.indexOf(card), 1);
+      player.setAttackCard = card;
+    }
+    else if(card.name == "Attack 25"){
+      player.hand.splice(player.hand.indexOf(card), 1);
+      player.setAttackCard = card;
+    }
+    else if(card.name == "Poke With Stick 0"){
+      player.hand.splice(player.hand.indexOf(card), 1);
+      player.setAttackCard = card;
+    }
+  }
 
   //Regular attack cards
   playAttackCards(player, creature){
     if(creature.hp[1] > 0){
-      console.log(creature.hp[1]);
       if(player.setAttackCard.name == "attack 40"){
         creature.hp[1] -= 40;
         if(creature.hp[1] <= 0 ){
@@ -32,13 +101,42 @@ export class DeckService {
     }
   }
 
-  //use Action card
-  playActionCards(player){
-
+  //DealCards
+  dealCards(localPlayers){
+    for(let player of localPlayers){
+      for(let i=0; i<7; i++){
+        player.hand.push(this.shuffleDeck[0]);
+        this.shuffleDeck.splice(0, 1);
+      }
+    }
+    return this.shuffleDeck;
   }
 
+  discard(player){
+    player.hand = ["cards"];
+    for(let i=0; i<7; i++){
+      player.hand.push(this.shuffleDeck[0]);
+      this.shuffleDeck.splice(0, 1);
+    }
+    return this.shuffleDeck;
+  }
+
+  drawToMaxHand(player){
+    if(player.hand.length < 8){
+      for(let i=player.hand.length; i < 8; i++){
+        player.hand.push(this.shuffleDeck[0]);
+        this.shuffleDeck.splice(0, 1);
+      }
+    }
+    else{
+      alert("Your hand is full.");
+    }
+    return this.shuffleDeck;
+  }
+
+  //use Action card
+  playActionCards(player: Player){
+    player.setActionCard = this.actionCard;
+    console.log(player);
+  }
 }
-
-
-
-
--- src/app/gameboard/gameboard.component.ts
+++ src/app/gameboard/gameboard.component.ts
@@ -22,6 +22,7 @@ export class GameboardComponent implements OnInit {
   players;
   cards;
   actionCard;
+  standbyItemCards;
   encounter;
   deck: any[] = [];
   shuffleDeck: any[] = [];
@@ -46,109 +47,27 @@ export class GameboardComponent implements OnInit {
     this.firebaseService.getCards().subscribe(response => {
       this.cards = response;
       //Retrieve cards from firebase
-      for(let card of this.cards[0][0].attackCards){
-        for(let i = 1; i < 26; i++){
-          this.deck.push(card);
-        }
-      }
-      for(let card of this.cards[0][1].actionCards){
-        for(let i = 1; i < 8; i++){
-          this.deck.push(card);
-        }
-      }
-      for(let card of this.cards[0][2].itemCards){
-        for(let i = 1; i < 8; i++){
-          this.deck.push(card);
-        }
-      }
-      //Populate deck
-      while(this.deck.length > 0){
-        let i = this.deck.length;
-        var randomNumber = Math.floor(Math.random() * i);
-        var singleCard = this.deck[randomNumber];
-        this.shuffleDeck.push(singleCard);
-        this.deck.splice(this.deck.indexOf(singleCard), 1);
-      }
-
-      console.log(this.shuffleDeck);
+      this.shuffleDeck = this.deckService.getCards(this.cards);
       //getInitiative, drawEncounter
       this.beginPhaseService.getInitiative(this.localPlayers);
     });
   }
 
   dealCards(){
-    for(let player of this.localPlayers){
-      for(let i=0; i<7; i++){
-        player.hand.push(this.shuffleDeck[0]);
-        this.shuffleDeck.splice(0, 1);
-      }
-    }
-
-    console.log(this.localPlayers);
-    console.log(this.shuffleDeck);
-  }
+    this.shuffleDeck = this.deckService.dealCards(this.localPlayers);
+   }
 
   discard(player: Player){
-    player.hand = ["cards"];
-    for(let i=0; i<7; i++){
-      player.hand.push(this.shuffleDeck[0]);
-      this.shuffleDeck.splice(0, 1);
-    }
-    console.log(this.shuffleDeck);
+    this.shuffleDeck = this.deckService.discard(player);
   }
-
   drawToMaxHand(player: Player){
-    if(player.hand.length < 8){
-      for(let i=player.hand.length; i < 8; i++){
-        player.hand.push(this.shuffleDeck[0]);
-        this.shuffleDeck.splice(0, 1);
-      }
-    }
-    else{
-      alert("Your hand is full.");
-    }
+    this.shuffleDeck = this.deckService.drawToMaxHand(player);
   }
 
-  useCard(card: any, player: Player){
-    //use potion card
-    if(card.name == "Potion Of Healing"){
-      if(player.hp == 100){
-        alert("This player's hp is full.");
-      }
-      else
-      {
-        player.hp += 25;
-        if(player.hp > 100){
-          player.hp = 100;
-        }
-        player.hand.splice(player.hand.indexOf(card), 1);
-      }
-    }
-    else if(card.name == "Edge Out" || card.name == "Mixed Signal"){
-      this.actionCard = card;
-      alert("Select a target.");
-      player.hand.splice(player.hand.indexOf(card), 1);
-    }
-    //set attack card
-    else if(card.name == "attack 40"){
-      player.hand.splice(player.hand.indexOf(card), 1);
-      player.setAttackCard = card;
-      console.log(player);
-      console.log(player.setAttackCard);
-    }
-    else if(card.name == "attack 25"){
-      player.hand.splice(player.hand.indexOf(card), 1);
-      player.setAttackCard = card;
-      console.log(player);
-      console.log(player.setAttackCard);
 
-    }
-    else if(card.name == "poke with stick 0"){
-      player.hand.splice(player.hand.indexOf(card), 1);
-      player.setAttackCard = card;
-      console.log(player);
-      console.log(player.setAttackCard);
-    }
+  //Set card in play
+  useCard(card: any, player: Player){
+    this.deckService.setCardInPlay(card, player);
   }
 
   //Use attack card
@@ -156,12 +75,8 @@ export class GameboardComponent implements OnInit {
     this.deckService.playAttackCards(player, this.encounter);
     player.setAttackCard = null;
   }
-
   //use action card
   useActionCard(player: Player){
-    player.setActionCard = this.actionCard;
-    console.log(this.actionCard);
+    this.deckService.playActionCards(player);
   }
-
-
 }
